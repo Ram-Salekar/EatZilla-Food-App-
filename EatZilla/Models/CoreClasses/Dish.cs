f94be7cd9a9bb94a67ecfaf24f605f7ac069bfd8
ramsalekar <ramsalekar4554@gmail.com>
2024-07-08 11:35:19 +0530

    core classes written

---
+++ EatZilla/Models/CoreClasses/Dish.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EatZilla.Models.CoreClasses
+{
+    public class Dish
+    {
+        [Key]
+        public int DishId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public byte[] image { get; set; }
+        [Required]
+        public int price {  get; set; }
+        public Dish()
+        {
+            
+        }
+        public Dish(int DisId,string name,int price)
+        {
+            this.DishId = DisId;
+            this.Name = name;
+            this.price = price;
+        }
+        public Dish(int DisId, string name, int price, byte[]img)
+        {
+            this.DishId = DisId;
+            this.Name = name;
+            this.price = price;
+            this.image = img;
+        }
+    }
+}
---
+++ EatZilla/Models/CoreClasses/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EatZilla.Models.CoreClasses
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public User()
+        {
+            
+        }
+        public User(int id,string name,string email,string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            
+        }
+
+    }
+}
