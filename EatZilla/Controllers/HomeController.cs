cffcda14f70ddf6972c25a687f7c353568fb6627
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-07-08 13:42:07 +0530

    done bu vishal

--- EatZilla/Controllers/HomeController.cs
+++ EatZilla/Controllers/HomeController.cs
@@ -28,6 +28,10 @@ namespace EatZilla.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
 
     }
